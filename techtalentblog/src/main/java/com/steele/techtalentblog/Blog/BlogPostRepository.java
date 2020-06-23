7b22703b6086e4676cc287bbc1dc52142b426763
Andre <jcsteele26@gmail.com>
2020-06-23 09:14:39 -0400

    Added Controller and Repository

---
+++ techtalentblog/src/main/java/com/steele/techtalentblog/Blog/BlogPostRepository.java
@@ -0,0 +1,9 @@
+package com.steele.techtalentblog.Blog;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
+
+
+    
+}
