6af8fd05996545677190d06d5320de6d97ea30bb
Andre <jcsteele26@gmail.com>
2020-06-23 08:23:35 -0400

    Blog Project Set Up

---
+++ techtalentblog/src/test/java/com/steele/techtalentblog/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steele.techtalentblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
