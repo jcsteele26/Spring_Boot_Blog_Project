70cc7eac419fbd1c382c382f4dbd424da9b06abf
Andre <jcsteele26@gmail.com>
2020-06-26 16:01:56 -0400

    Corrected errors in Blog Post Controller

--- techtalentblog/src/main/java/com/steele/techtalentblog/BlogPost/BlogPostController.java
+++ techtalentblog/src/main/java/com/steele/techtalentblog/BlogPost/BlogPostController.java
@@ -2,12 +2,16 @@ package com.steele.techtalentblog.BlogPost;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BlogPostController {
@@ -23,7 +27,7 @@ public class BlogPostController {
         for(BlogPost post : blogPostRepository.findAll()) {
             posts.add(post);
         }
-        //model.addAttribute("posts", posts);
+        model.addAttribute("posts", posts);
         return "blogpost/index";
     }
 
@@ -36,17 +40,56 @@ public class BlogPostController {
 
     @PostMapping(value = "/blogposts")
     public String addNewBlogPost(BlogPost blogPost, Model model) {
-        blogPostRepository.save(new BlogPost(blogPost.getTitle(), blogPost.getAuthor(), blogPost.getBlogEntry()));
+        blogPostRepository.save(blogPost);
         
         //Add new blog posts as they are created
         //posts.add(blogPost);
 
         //Add attributes to model to view in results page
-        model.addAttribute("title", blogPost.getTitle());
-        model.addAttribute("author", blogPost.getAuthor());
-        model.addAttribute("blogEntry", blogPost.getBlogEntry());
+        model.addAttribute("blogPost", blogPost);
+       // model.addAttribute("title", blogPost.getTitle());
+      //  model.addAttribute("author", blogPost.getAuthor());
+      //  model.addAttribute("blogEntry", blogPost.getBlogEntry());
 
         return "blogpost/result";
 
     }
+
+    @RequestMapping(value = "/blogposts/{id}", method= RequestMethod.GET)
+    public String editPostWithId(@PathVariable Long id, BlogPost blogPost, Model model){
+        Optional<BlogPost> post= blogPostRepository.findById(id);
+        if(post.isPresent()){
+            BlogPost actualPost=post.get();
+            model.addAttribute("blogPost", actualPost);
+        }
+
+        return "blogpost/edit";
+    }
+
+    @RequestMapping(value = "/blogposts/update/{id}", method = RequestMethod.POST)
+    public String updateExistingPost(@PathVariable Long id, BlogPost blogPost, Model model){
+        Optional<BlogPost> post = blogPostRepository.findById(id);
+        if(post.isPresent()){
+            BlogPost actualPost = post.get();
+            actualPost.setTitle(blogPost.getTitle());
+            actualPost.setAuthor(blogPost.getAuthor());
+            actualPost.setBlogEntry(blogPost.getBlogEntry());
+
+            blogPostRepository.save(actualPost);
+            //model.addAttribute("blogPost", actualPost);
+            model.addAttribute("title", actualPost.getTitle());
+            model.addAttribute("author", actualPost.getAuthor());
+            model.addAttribute("blogEntry", actualPost.getBlogEntry());
+        }
+
+        return "blogpost/result";
+    }
+
+    @RequestMapping(value = "blogposts/delete/{id}")
+    public String deletePostById(@PathVariable Long id) {
+        blogPostRepository.deleteById(id);
+        return "blogpost/delete";
+    }
+
+
 }
